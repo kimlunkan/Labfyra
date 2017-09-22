3bc513b1213731a8bcf6adf10ca4907c810574cf
Kim Lundqvist <lunkan_17@hotmail.com>
2017-09-22 15:30:00 +0200

    Removed test class "Hej"

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 
 namespace Lab4
-{
-    public class Hej
-    {
-
-    }
+{   
     class Program
     {
         static void Main(string[] args)
         {
-
-
 
         }
     }
