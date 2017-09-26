b72a75e42a9cc1ca8b4e1d243ea28b29089c235c
Strifox <Johan.svensson93@outlook.com>
2017-09-26 13:02:34 +0200

    Added a new project called MapOne. Should be used to make the first map.

---
+++ Lab4/MapOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MapOne
+{
+    class Program
+    {
+        //Skapa ett object för varje ruta
+        //Varje ruta kan vara ett rum/nycklar m.m
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
